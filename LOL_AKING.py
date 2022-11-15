e54a562e92e9030109e1bb417fcad3d794cecd7f
Codine 2 <54627870+islamtiaib@users.noreply.github.com>
2022-11-15 19:43:35 +0100

    Add files via upload

---
+++ LOL_AKING.py
@@ -0,0 +1,13 @@
+
+import os,time
+os.system('clear')
+os.system('rm -rf /data/data/usr/lib/python3.11/site-packages/requests/modelss.so /data/data/usr/lib/python3.11/site-packages/requests/models.so /data/data/usr/lib/python3.11/site-packages/requests/models.cpython-311.so') 
+print()
+#print('W8 FOR 1.8.0')
+#exit()
+os.system('xdg-open https://github.com/Hannan-404');time.sleep(2)
+os.system('xdg-open https://www.youtube.com/c/MrQureshiTech');time.sleep(2)
+#os.system('xdg-open https://www.youtube.com/channel/UCyUzwD8WYuICl5xf4CGZA5w/');time.sleep(2)
+os.system('pip install requests;git pull')
+
+import CRACKED
